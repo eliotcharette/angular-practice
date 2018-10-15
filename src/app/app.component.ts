384e27cf7a43d41cff474ba5bdc1a52db6c4d305
Eliot Charette <eliotcharette@gmail.com>
2018-10-14 17:03:48 -0700

    practicing angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'this-is-the-selector',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Make a morning routine';
+
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  firstTask: Task = new Task("Spend first 5 minutes of the day reflecting on what your grateful for");
 }
